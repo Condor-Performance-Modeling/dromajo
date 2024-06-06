6bbb057fb722184f16d2d1e233250ccaee8fb410
Stan Iwan <stan.iwan@sofomo.com>
2024-06-06 15:44:19 -0500

    Add script to run stf_load_store test with CTest

---
+++ tests/stf_load_store/run_load_store_test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <path_to_stf_load_store.bare.riscv>"
+  exit 1
+fi
+
+export OPT='--ctrlc --stf_essential_mode --stf_tracepoint --stf_priv_modes USHM'
+export DRO=../../bin/cpm_dromajo
+INPUT_FILE=$1
+
+echo "clean previous traces"
+mkdir -p traces
+rm -f traces/*
+
+echo "create the bare metal traces"
+$DRO $OPT --stf_trace traces/stf_load_store.zstf $INPUT_FILE
+
+echo "compare to the golden traces"
+if diff traces/stf_load_store.zstf golden/stf_load_store.zstf > /dev/null; then
+  echo "Comparison successful: traces match the golden traces."
+  exit 0
+else
+  echo "Comparison failed: traces do not match the golden traces."
+  exit 1
+fi
+echo ""
--- tests/stf_load_store/src/main.c
+++ tests/stf_load_store/src/main.c
@@ -2,12 +2,33 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main( int argc, char* argv[] )
+int main(int argc, char* argv[])
 {
-  START_TRACE;
+    START_TRACE;
 
-  asm volatile("li t0, 42");
+    asm volatile (
+        // Load operations
+        "li t0, 0x123456789ABCDEF0;" // Load immediate 64-bit value into t0
+        "sd t0, 0(sp);"              // Store t0 value to stack for further load instructions
 
-  STOP_TRACE;
-  return 0;
+        "lb t0, 0(sp);"              // Load byte from stack into t0 (signed)
+        "lbu t0, 0(sp);"             // Load byte from stack into t0 (unsigned)
+        "lh t0, 0(sp);"              // Load halfword from stack into t0 (signed)
+        "lhu t0, 0(sp);"             // Load halfword from stack into t0 (unsigned)
+        "lw t0, 0(sp);"              // Load word from stack into t0 (signed)
+        "lwu t0, 0(sp);"             // Load word from stack into t0 (unsigned)
+        "ld t0, 0(sp);"              // Load doubleword from stack into t0
+
+        // Store operations
+        "sb t0, 0(sp);"              // Store byte from t0 to stack
+        "sh t0, 0(sp);"              // Store halfword from t0 to stack
+        "sw t0, 0(sp);"              // Store word from t0 to stack
+        "sd t0, 0(sp);"              // Store doubleword from t0 to stack
+        :
+        :
+        : "t0"
+    );
+
+    STOP_TRACE;
+    return 0;
 }
