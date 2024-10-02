c38febbc6021929736ebf5a30525c864fc23d7b9
Stan Iwan <stan.iwan@sofomo.com>
2024-10-02 10:12:14 -0500

    Fix stf_load_store checks

--- tests/stf_load_store/scripts/check_riscv_env_variable.sh
+++ tests/stf_load_store/scripts/check_riscv_env_variable.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <env_variable_name>"
-    exit 1
-fi
-
-ENV_VAR_NAME=$1
-
-ENV_VAR_VALUE=$(eval echo \$$ENV_VAR_NAME)
-
-if [ -z "$ENV_VAR_VALUE" ]; then
-    echo "Error: $ENV_VAR_NAME environment variable is not set."
+if [ -z "$RISCV" ]; then
+    echo "Error: RISCV environment variable is not set."
     echo "Please set it to the RISC-V toolchain installation directory."
-    echo "IMPORTANT! After setting the variable," \
-    "remember to reconfigure the build using cmake .."
+    echo "Example: export RISCV=/data/tools/riscv64-unknown-elf"
+    echo "IMPORTANT! After setting the variable, remember to run cmake .. in your build directory!"
     exit 1
 fi
 
-echo "$ENV_VAR_NAME environment variable is set to: $ENV_VAR_VALUE"
-echo "IMPORTANT! After setting the variable," \
-"remember to reconfigure the build using cmake .."
+echo "RISCV environment variable is set to: $RISCV"
+echo "IMPORTANT! After setting the variable, remember to run cmake .. in your build directory!"
 exit 0
