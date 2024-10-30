a0eefde4c08d94b510860614fe77c906c0090b96
Stan Iwan <stan.iwan@sofomo.com>
2024-10-30 09:18:18 -0500

    Compare log instead of stf traces

--- tests/reset_vector/run_test.sh
+++ tests/reset_vector/run_test.sh
@@ -17,19 +17,20 @@ echo "Creating the bare metal traces..."
 for INPUT_FILE in $BIN_DIR/*.riscv; do
     base_name=$(basename "$INPUT_FILE")
     
+    echo ""
     echo "Processing file: $INPUT_FILE"
-    $DRO $RESET_VECTOR_OPT $OPT --stf_trace traces/"$base_name".zstf "$INPUT_FILE"
+    $DRO $RESET_VECTOR_OPT $OPT --exe_trace 0 --exe_trace_log traces/"$base_name".log "$INPUT_FILE"
     
-    if [ -f "$GOLDEN_DIR/$base_name".zstf ]; then
+    if [ -f "$GOLDEN_DIR/$base_name".log ]; then
         echo "Comparing binary trace files for $base_name..."
-        if diff traces/"$base_name".zstf "$GOLDEN_DIR/$base_name".zstf > /dev/null; then
+        if diff traces/"$base_name".log "$GOLDEN_DIR/$base_name".log > /dev/null; then
             echo "Comparison successful for $base_name: binary traces match the golden traces."
         else
             echo "Comparison failed for $base_name: binary traces do not match the golden traces."
             fail_count=$((fail_count+1))
         fi
     else
-        echo "Warning: No golden trace file found for $base_name."
+        echo "Warning: No golden trace file found for $base_name"
         fail_count=$((fail_count+1))
     fi
 done
