0eecf2c33e0af6cc5a1d3dfe134020ed873a6c7a
David Mielke <david.mielke@sifive.com>
2024-10-31 14:47:12 -0500

    Add ZICOND instructions

--- include/dromajo_template.h
+++ include/dromajo_template.h
@@ -61,6 +61,7 @@
 //#define EN_ZBC (s->machine->common.ext_flags.zbb == true)
 //#define EN_ZBS (s->machine->common.ext_flags.zbs == true)
 
+#define EN_ZICOND 1
 #define EN_ZBA 1
 #define EN_ZBB 1
 #define EN_ZBC 1
@@ -1849,6 +1850,16 @@ int no_inline glue(riscv_cpu_interp, XLEN)(RISCVCPUState *s, int n_cycles) {
 
                     CAPTURED_INSTR("BINV");
                     val = (uintx_t)val ^ ((uintx_t)1 << (val2 & (XLEN - 1)));
+
+                } else if (EN_ZICOND && _funct7 == 0x07 && _funct3 == 0x5) {
+
+                    CAPTURED_INSTR("CZERO.EQZ");
+                    val = (uintx_t)val2 == 0 ? (uintx_t)0 : (uintx_t)val;
+ 
+                } else if (EN_ZICOND && _funct7 == 0x07 && _funct3 == 0x7) {
+
+                    CAPTURED_INSTR("CZERO.NEZ");
+                    val = (uintx_t)val2 != 0 ? (uintx_t)0 : (uintx_t)val;
 
                 } else if (imm == 1) {
                     //TODO CAN BE REMOVED - this is the original code before zbb
